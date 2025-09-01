79c96ad862b4246030ae6322227521f8e4e96245
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-01 17:05:09 +0600

    Load videos, display cards abd display dynamic data to card

--- scripts/video.js
+++ scripts/video.js
@@ -13,10 +13,80 @@ const loadCategories = () =>{
 };
 
 
+const loadVideos = () =>{
+   // console.log("loadCategories is connected");
+      //fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data)=>displayVideos(data.videos))
+    .catch((error)=> console.log(error))
+};
+
+const displayVideos = (videos) =>{
+  // console.log(videos)
+    const videoContainer = document.getElementById("videos");
+
+  videos.forEach(video => {
+    console.log(video);
+    //create a card for each video
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    
+     <figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-3">
+    <div>
+    <img src =${video.authors[0].profile_picture} class="w-10 h-10 rounded-full" object-cover />
+    </div>
+    <div>
+    <h2 class="card-title text-sm font-bold">${video.title}</h2>
+    <div class="flex  items-center">
+      <p class="text-xs text-gray-500">${video.authors[0].profile_name}</p>
+      <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+      
+    </div>
+
+    </div
+
+
+
+    </div>
+  </div>
+  `;
+
+  videoContainer.append(card);
+
+  });
+};
+
  
 //   {
 //     "category_id": "1001",
 //     "category": "Music"
+// }
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaab",
+//     "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+//     "title": "Midnight Serenade",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
+//             "profile_name": "Noah Walker",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "543K",
+//         "posted_date": ""
+//     },
+//     "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
 // }
 
 
@@ -41,4 +111,6 @@ const displayCategories = (categories) =>{
 };
  
 loadCategories() 
+loadVideos()
+
 
