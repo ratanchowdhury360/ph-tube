7bea0046c9e0b7535d045fba6c4632fc35d830ad
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-03 00:11:27 +0600

    Load And Display Videos Based On Category

--- scripts/video.js
+++ scripts/video.js
@@ -2,12 +2,12 @@ console.log("video script added");
 
 //time converter function
 function getTimeString(time) {
-    const hour = parseInt(time / 3600);
-    let remainingSecond = time % 3600;
-    const minute = parseInt(remainingSecond / 60);
-    remainingSecond = remainingSecond % 60;
-    const second = remainingSecond;
-    return `${hour} hour ${minute} min ${second} sec ago`;
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  const second = remainingSecond;
+  return `${hour} hour ${minute} min ${second} sec ago`;
 }
 
 
@@ -17,28 +17,37 @@ function getTimeString(time) {
 
 //create loadCategories
 
-const loadCategories = () =>{
-   // console.log("loadCategories is connected");
-      //fetch the data
-    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+const loadCategories = () => {
+  // console.log("loadCategories is connected");
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data)=>displayCategories(data.categories))
-    .catch((error)=> console.log(error))
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error))
 };
 
 
-const loadVideos = () =>{
-   // console.log("loadCategories is connected");
-      //fetch the data
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = () => {
+  // console.log("loadCategories is connected");
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data)=>displayVideos(data.videos))
-    .catch((error)=> console.log(error))
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error))
 };
 
-const displayVideos = (videos) =>{
+const loadCategoryVideos = (id) => {
+  //alert(id);
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error))
+}
+
+const displayVideos = (videos) => {
   // console.log(videos)
-    const videoContainer = document.getElementById("videos");
+  const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
 
   videos.forEach(video => {
     console.log(video);
@@ -52,10 +61,8 @@ const displayVideos = (videos) =>{
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-      ${
-        video.others.posted_date?.length ==0 ? "" : ` <span class="absolute  bottom-20 right-2 bg-black text-white  rounded p-1">${
-       getTimeString(video.others.posted_date)
-      }</span>`
+      ${video.others.posted_date?.length == 0 ? "" : ` <span class="absolute text-xs  bottom-20 right-2 bg-black text-white  rounded p-1">${getTimeString(video.others.posted_date)
+        }</span>`
       }
      
   </figure>
@@ -67,8 +74,7 @@ const displayVideos = (videos) =>{
     <h2 class="card-title text-sm font-bold">${video.title}</h2>
     <div class="flex gap-1 items-center">
       <p class="text-xs text-gray-500">${video.authors[0].profile_name}</p>
-      ${
-        video.authors[0].verified == true ? `<img class="w-5"  src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : ``  }
+      ${video.authors[0].verified == true ? `<img class="w-5"  src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : ``}
       
     </div>
 
@@ -80,12 +86,12 @@ const displayVideos = (videos) =>{
   </div>
   `;
 
-  videoContainer.append(card);
+    videoContainer.append(card);
 
   });
 };
 
- 
+
 //   {
 //     "category_id": "1001",
 //     "category": "Music"
@@ -112,26 +118,29 @@ const displayVideos = (videos) =>{
 
 
 
-const displayCategories = (categories) =>{
+const displayCategories = (categories) => {
   //  console.log( "displayCategories is connected");
- // console.log(data)
+  // console.log(data)
   const categoriesContainer = document.getElementById("categories");
 
   categories.forEach((item) => {
     console.log(item);
 
     //create a button for each category
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
+      <button onclick="loadCategoryVideos(${item.category_id})" class ="btn">
+      ${item.category}
+      </button>
+   `;
 
     //add button to the categories container
-    categoriesContainer.append(button);
-    
+    categoriesContainer.append(buttonContainer);
+
   });
 };
- 
-loadCategories() 
+
+loadCategories()
 loadVideos()
 
 
